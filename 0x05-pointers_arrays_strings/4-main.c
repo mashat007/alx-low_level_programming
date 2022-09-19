3a6d90cf4b178d1b011975d80e925480a43efd7f
= <=>
2022-09-19 11:20:30 -0700

    Reverse print

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Two things are infinite, the universe and human stupidity - Albert E";
+	print_rev(str);
+	return (0);
+}
